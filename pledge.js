2f34d04ea63bc09a0532b1f95aefcaae9018bd17
Elliott McNary <El_Is_Based@MacBook-Pro.local>
2016-06-22 16:34:48 -0400

    first chapter done

--- pledge.js
+++ pledge.js
@@ -4,7 +4,36 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+var $Promise = function(){
+  this.state = 'pending';
+  this.value = null;
 
+};
+
+var Deferral = function(){
+  this.$promise = new $Promise();
+};
+
+var defer = function(){
+  return new Deferral();
+};
+
+Deferral.prototype.resolve = function(data){
+  if(this.$promise.state !== 'pending'){
+    return;
+  } else {
+    this.$promise.value = data;
+    this.$promise.state = 'resolved';
+  }
+};
+Deferral.prototype.reject = function(reason){
+  if(this.$promise.state !== 'pending'){
+    return;
+  } else {
+    this.$promise.state = 'rejected';
+    this.$promise.value = reason;
+  }
+};
 
 
 
